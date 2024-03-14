1994873620b08e884fcc353e2d016e59a56df69d
Luong Ba Hoang <hoang.luongba@vti.com.vn>
2024-03-14 11:12:23 +0700

    update

---
+++ roles/system_health_check/files/task.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+system_health_check_target=""
+system_health_check_wait=3
+system_health_check_status="OK"
+system_health_check_time=3
+
+while true
+do
+  sleep $system_health_check_wait
+#  current_alarm_state=$(aws cloudwatch describe-alarms --query "MetricAlarms[?contains(AlarmName, \`${system_health_check_target}\`) == \`true\`]" | jq .[0].StateValue)
+  current_alarm_state=$(echo 'OK')
+
+  if [ $current_alarm_state == "${system_health_check_status}" ]
+  then
+    echo 'SUCCESS'
+    exit 0
+  fi
+
+  system_health_check_time=$((system_health_check_time - 1))
+  echo "${system_health_check_time}"
+
+  if [ $system_health_check_time -eq 0 ]
+  then
+    echo 'ERROR'
+    exit 1
+  fi
+
+done
+
+
