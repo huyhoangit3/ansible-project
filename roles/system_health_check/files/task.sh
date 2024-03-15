8643d7e5ee910a7ffb9fe20e9cc8ac0e4370a028
Luong Ba Hoang <hoang.luongba@vti.com.vn>
2024-03-15 10:05:37 +0700

    update

--- roles/system_health_check/files/task.sh
+++ roles/system_health_check/files/task.sh
@@ -13,7 +13,8 @@ do
 #  current_alarm_state=$(echo 'OK')
   echo $current_alarm_state
 
-  if [[ "$current_alarm_state" == "$system_health_check_status" ]];
+#  if [[ "$current_alarm_state" == "$system_health_check_status" ]];
+  if [[ "$current_alarm_state" == "OK" ]];
   then
     echo 'SUCCESS'
     exit 0
